293b53f4485f5ad6ab4a27a24db3d4f7477e826b
Chris <elhalabichristian@gmail.com>
2024-05-17 16:43:04 +0300

    Adding Data View

--- src/app/actor-search/actor-search.component.ts
+++ src/app/actor-search/actor-search.component.ts
@@ -1,21 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
+import { DataViewModule } from 'primeng/dataview';
 import { DialogService } from 'primeng/dynamicdialog';
 import { InputTextModule } from 'primeng/inputtext';
+import { PaginatorModule } from 'primeng/paginator';
+import { TabViewModule } from 'primeng/tabview';
 import { ToastModule } from 'primeng/toast';
 import { ActorDetailsComponent } from '../actor-details/actor-details.component';
 import { FavoriteActorsService } from '../favorite-actors.service';
 import { TmdbService } from '../tmdb.service';
-
 @Component({
   selector: 'app-actor-search',
   standalone: true,
   templateUrl: './actor-search.component.html',
-  styleUrl: './actor-search.component.scss',
+  styleUrls: ['./actor-search.component.scss'],
   imports: [
     CommonModule,
     FormsModule,
@@ -23,14 +25,20 @@ import { TmdbService } from '../tmdb.service';
     InputTextModule,
     ToastModule,
     CarouselModule,
+    DataViewModule,
+    PaginatorModule,
+    TabViewModule,
   ],
   providers: [DialogService, MessageService],
 })
-export class ActorSearchComponent implements OnInit {
+export class ActorSearchComponent {
   query: string = '';
   actors: any[] = [];
+  paginatedActors: any[] = [];
   selectedActor: any = null;
   responsiveOptions: any[] | undefined;
+  totalRecords: number = 0;
+  rows: number = 9;
 
   constructor(
     private tmdbService: TmdbService,
@@ -40,32 +48,19 @@ export class ActorSearchComponent implements OnInit {
     private cdr: ChangeDetectorRef
   ) {}
 
-  ngOnInit() {
-    this.responsiveOptions = [
-      {
-        breakpoint: '1199px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '991px',
-        numVisible: 2,
-        numScroll: 1,
-      },
-      {
-        breakpoint: '767px',
-        numVisible: 1,
-        numScroll: 1,
-      },
-    ];
-  }
-
   searchActors() {
     if (this.query.trim()) {
       this.tmdbService.searchActors(this.query).subscribe((data: any) => {
         this.actors = data.results;
+        this.totalRecords = this.actors.length;
+        this.paginate({ first: 0, rows: this.rows });
       });
     }
+  }
+
+  paginate(event: any) {
+    const { first, rows } = event;
+    this.paginatedActors = this.actors.slice(first, first + rows);
   }
 
   selectActor(actor: any) {
